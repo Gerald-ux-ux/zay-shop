6522b2224bad5deff6ae898d944cbae5edb68073
Gerald-ux-ux <11geraldmoney@gmail.com>
2023-01-24 20:13:48 +0300

    store.js issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import "../styles/Navbar.css";
+import Product from "./pages/Product";
 
 const Navbar = () => {
-  const state = useSelector ((state) => state.handleCart)
+  const state = useSelector((state) => state.handleCart);
+  const [filteredProduct, setFilteredProduct] = useState([]);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  function handleSelect(product) {
+    window.location.href = `/product/${product.name}`;
+  }
+const [searchTerm, setSearchTerm] = useState('');
+const handleSearch = (event) => {
+  setSearchTerm(event.target.value);
+  const filtered = product.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setFilteredProduct(filtered);
+};
+
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg   py-3 shadow-sm ">
@@ -54,7 +85,14 @@ const Navbar = () => {
                 type="search"
                 placeholder="Search for items..."
                 aria-label="Search"
+                onChange={handleSearch}
               />
+
+              {filteredProduct.slice(0, 3).map((product) => (
+                <li key={product.id} onClick={() => handleSelect(product)}>
+                  {product.Name}
+                </li>
+              ))}
               <button class="btn btn-light " type="submit">
                 Search
               </button>
